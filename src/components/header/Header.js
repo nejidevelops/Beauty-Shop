c411c03cebea0c1c43bfd61f950bbb33633ae4ea
Newton Ombese <newtonombese1@gmail.com>
2022-12-17 07:04:06 +0300

    TEST 1 { IF EVERYTHING IS DISPLAYED AFTER NPM START}

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import css from './Header.module.css'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className={css.container}>
+        <div className={css.logo}>LOGO</div>
+    </div>
   )
 }
 
