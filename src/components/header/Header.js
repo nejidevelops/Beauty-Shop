6a477ba31374998bc8f96b488f087727a3b95d3a
Newton Ombese <newtonombese1@gmail.com>
2022-12-17 07:23:21 +0300

    the header is now getting shape as it is now looking better

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import css from './Header.module.css'
+import Logo from "../../assets/logo.png"
 
 function Header() {
   return (
     <div className={css.container}>
-        <div className={css.logo}>LOGO</div>
+        <div className={css.logo}>
+          <img src={Logo} alt="logo" />
+          <span>Bofulo</span>
+        </div>
+        <div className={css.right}>
+            <div className={css.menu}>
+              
+            </div>
+            <input type="text" className={css.search} />
+            <span>PNG Icon of Cart</span>
+        </div>
     </div>
   )
 }
